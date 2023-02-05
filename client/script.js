308f9050f61e9d8871101f042c8abacd48bc169e
Jordan Wadsworth <jordanwadsworth@jordans-mbp-2.lan>
2023-02-05 22:15:12 +1000

    scroll when page is filled

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ import bot from './assets/wAIv.svg'
 import user from './assets/user.svg'
 
 const form = document.querySelector('form')
-const chatContainer = document.querySelector('#chat_container')
+const chatContainer = document.querySelector('chat_container')
 
 let loadInterval
 let isRunning = false
@@ -130,8 +130,7 @@ const handleSubmit = async (e) => {
     chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
     // to focus scroll to the bottom 
-    chatContainer.scrollBottom = chatContainer.scrollHeight
-    chatContainer.scrollBottom
+    chatContainer.scrollTop = chatContainer.scrollHeight
 
     // specific message div 
     const messageDiv = document.getElementById(uniqueId)
