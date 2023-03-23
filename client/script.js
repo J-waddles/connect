9cdec3b1b78c73a5b45d52ed99b43b73bd83bc78
Jordan Wadsworth <jordanwadsworth@jordans-mbp-2.lan>
2023-03-23 16:10:13 +1000

    update server

--- client/script.js
+++ client/script.js
@@ -119,7 +119,7 @@ const handleSubmit = async (e) => {
     form.querySelector('input').blur();
 
     // change
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://waiv.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
